ef29388bbda9fd9dc75dd3d694012eb07f83399c
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-10 18:44:56 +0530

    New code

--- lib/puppet/functions/saymyname.rb
+++ lib/puppet/functions/saymyname.rb
@@ -1,7 +1,7 @@
 Puppet::Functions.create_function(:'gppuppet::saymyname') do
-  dispatch :saymyname do
-    param 'String', :myname
-  end
+  # dispatch :saymyname do
+  #   param 'String', :myname
+  # end
   def saymyname(myname) do
     "My name is #{myname}"
   end
