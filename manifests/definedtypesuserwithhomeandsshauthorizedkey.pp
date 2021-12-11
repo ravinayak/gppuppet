2ae123950a06ac42da74ea76c875425770d498d1
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-12 03:54:20 +0530

    New code

---
+++ manifests/definedtypesuserwithhomeandsshauthorizedkey.pp
@@ -0,0 +1,37 @@
+define gppuppet::definedtypesuserwithhomeandsshauthorizedkey(
+  String $username,
+  String $fileforuserinhomedir,
+  String $key,
+){
+  $groups = ['newgroup', 'definedtypegroup']
+  user { $username:
+    ensure     => present,
+    managehome => false,
+    system     => false,
+    uid        => 10002,
+    gid        => 5001,
+    groups     => $groups,
+    allowdupe  => false,
+    shell      => '/bin/bash',
+  }
+  group { $groups[0]:
+    ensure => present,
+    gid    => 5002,
+  }
+  group { $groups[1]:
+    ensure => present,
+    gid    => 5003,
+  }
+  file {"/home/${username}":
+    ensure => directory,
+    owner  => $username,
+    group  => $username,
+    mode   => '0755'
+  }
+  ssh_authorized_key { "${username}@localhost":
+    ensure => present,
+    user   => $username,
+    type   => key,
+    key    => $key,
+  }
+}
