49d16476c0ec1ec49a2d003f4a1beeaf6505d0b8
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-11 17:35:03 +0530

    New code

--- functions/production/parsepuppetenv.pp
+++
@@ -1,9 +0,0 @@
-function parsepuppetenv(String $hostname) {
-  $env = $hostname ? {
-    /[^\-]+-p-\d+/ => 'Production',
-    /[^\-]+-d-\d+/ => 'Development',
-    /[^\-]+-q-\d+/ => 'QA',
-    default        => 'Unknown Environment',
-  }
-  notice("My environment is :: ${env}")
-}
