90144ac44f4fe877a8a475f0354186ed4766b98b
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-13 11:36:02 +0530

    New code

--- manifests/sampleclasswithfileresources.pp
+++ manifests/sampleclasswithfileresources.pp
@@ -1,16 +1,14 @@
 class gppuppet::sampleclasswithfileresources{
   file {'/etc/samplefiletest1.txt':
-    ensure  => file,
-    owner   => 'neo1',
-    group   => 'neo1',
-    mode    => '0777',
-    content => "Sample File -1",
+    ensure => file,
+    owner  => 'neo1',
+    group  => 'neo1',
+    mode   => '0777',
   }
   file {'/etc/samplefiletest2.txt':
-    ensure  => file,
-    owner   => 'vagrant',
-    group   => 'vagrant',
-    mode    => '0755',
-    content => 'Sample File -2',
+    ensure => file,
+    owner  => 'vagrant',
+    group  => 'vagrant',
+    mode   => '0755',
   }
 }
