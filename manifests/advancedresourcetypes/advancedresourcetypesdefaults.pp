ea33f6185c1a92b1604921db36b272310e4a0147
Ravi Kumar Nayak <ravinayak19@gmail.com>
2021-12-16 11:54:10 +0530

    New code

--- manifests/advancedresourcetypes/advancedresourcetypesdefaults.pp
+++ manifests/advancedresourcetypes/advancedresourcetypesdefaults.pp
@@ -32,7 +32,7 @@ class gppuppet::advancedresourcetypes::advancedresourcetypesdefaults{
     'default1': ;
     'default2': ;
   }
-  Defaultsdefinedtypewithinclass <| tag == defaulttag |>.each |$dt_inst| {
+  gppuppet::advancedresourcetypes::advancedresourcetypesdefaults::Defaultsdefinedtypewithinclass <| tag == defaulttag |>.each |$dt_inst| {
     notice($dt_inst)
     defaultdefinedtypewithinclass { $dt_inst:
       tag => 'nomoredefaulttag',
